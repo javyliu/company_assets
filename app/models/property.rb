c5963db329afa0deab1cd187713607a94e534827
javy_liu <javy_liu@163.com>
2013-01-21 17:06:10 +0800

    update files

--- app/models/property.rb
+++ app/models/property.rb
@@ -9,10 +9,10 @@
 
 class Property < ActiveRecord::Base
   default_scope order("id")
-  attr_accessible :name, :style
+  attr_accessible :name, :style,:width
   has_many :check_options
   has_many :category_properties
-  validates_presence_of :name,:style,:width
+  validates_presence_of :name,:style
   #scope :used,includes(:category_properties).where(:category_properties => {:int_value => nil})
 
   Style = [["字符类型",0],["单选类型",1]]
